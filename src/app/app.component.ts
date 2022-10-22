a43f63b853d5bf9b42e677de80cbd1df1812065c
Pedro Henrique Cursino Cabral <97694134+WorenoKing@users.noreply.github.com>
2022-10-21 21:30:19 -0300

    trabalho 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabalho';
+  title = 'Trabalho de Angular 2';
+  aluno = "Pedro Cabral";
+
+  imagens = [
+    "https://cdn.pixabay.com/photo/2022/10/15/21/23/cat-7523894_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/07/15/09/00/man-845847_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/04/23/12/32/hell-735995_960_720.jpg",
+    "https://media.istockphoto.com/photos/global-communication-network-concept-social-media-worldwide-business-picture-id1204762097?s=612x612"
+  ]
+
+
+
 }
