57f7f10170ef1085c7c03c781f2e06a0830b018d
Pedro Henrique Cursino Cabral <97694134+WorenoKing@users.noreply.github.com>
2022-10-21 21:32:04 -0300

    Trabalho 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Trabalho de Angular 2';
+  title = 'Trabalho de Angular 3';
   aluno = "Pedro Cabral";
 
   imagens = [
